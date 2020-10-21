9c35a6ad826f7c13b302b375eae08e2dbc15bb85
Julien Stroheker <juliens@microsoft.com>
2020-10-21 18:11:58 -0400

    Vendoring latest changes (#4)
    * WIP
    
    * initial fake updates
    
    * update fake templates
    
    * review cleanups
    
    * cleanup
    
    * implement this interface better?
    
    * allow setting as unavailable
    
    * don't return nil from queryall
    
    * pass in options to queries
    
    * unique keys
    
    * fix unique keys
    
    * update
    
    * add sorting functionality
    
    * update from review comments
    
    * guard all fields of FakeTemplateClient under lock
    
    * avoid unneeded allocations in {decode,encode}Template
    
    * simplification
    
    * remove O(N) scan on create/update if checkDocsConflict is nil
    
    * remove naughty use of reflect
    
    * remove Client from some type names so that they are less terrifyingly long
    
    * allow the nil sorter, this eases unsetting the sorter if ever required
    
    * rename fields and variable names for clarity/standardisation
    
    * fix out go vet documentation requirements
    
    * use gofmt in generate.go
    
    * fix logic error by removing encodeAndCopy
    
    * Make the iterator a raw-able one again to meet the interface and expose it for queries
    
    * fix merge issue
    
    Co-authored-by: Jim Minter <jminter@redhat.com>
    Co-authored-by: Amber Brown <ambrown@redhat.com>
    Co-authored-by: Jim Minter <jim-minter@users.noreply.github.com>

--- example/cosmosdb/generate.go
+++ example/cosmosdb/generate.go
@@ -1,3 +1,4 @@
 package cosmosdb
 
 //go:generate go run github.com/julienstroheker/go-cosmosdb/cmd/gencosmosdb github.com/julienstroheker/go-cosmosdb/example/types,Person,People
+//go:generate gofmt -s -w .
