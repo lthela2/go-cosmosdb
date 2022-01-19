9d644351005773ab11ea05345bd8deb4b1699643
Shruti Ramesh <sshrutz@gmail.com>
2022-01-19 12:13:03 -0800

    Add Oauth Authorizer to Enable RBAC Authentication for CosmosDB (#14)

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/julienstroheker/go-cosmosdb
 go 1.14
 
 require (
+	github.com/Azure/go-autorest/autorest/adal v0.9.11
 	github.com/sirupsen/logrus v1.7.0
 	github.com/ugorji/go/codec v1.2.0
 	golang.org/x/sys v0.0.0-20201116194326-cc9327a14d48 // indirect
--- pkg/gencosmosdb/cosmosdb/authorizer.go
+++ pkg/gencosmosdb/cosmosdb/authorizer.go
@@ -1,6 +1,7 @@
 package cosmosdb
 
 import (
+	"context"
 	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/base64"
@@ -9,17 +10,19 @@ import (
 	"net/url"
 	"strings"
 	"time"
+
+	"github.com/Azure/go-autorest/autorest/adal"
 )
 
 type Authorizer interface {
-	Authorize(*http.Request, string, string)
+	Authorize(context.Context, *http.Request, string, string)
 }
 
 type masterKeyAuthorizer struct {
 	masterKey []byte
 }
 
-func (a *masterKeyAuthorizer) Authorize(req *http.Request, resourceType, resourceLink string) {
+func (a *masterKeyAuthorizer) Authorize(ctx context.Context, req *http.Request, resourceType, resourceLink string) {
 	date := time.Now().UTC().Format("Mon, 02 Jan 2006 15:04:05 GMT")
 
 	h := hmac.New(sha256.New, a.masterKey)
@@ -42,10 +45,40 @@ type tokenAuthorizer struct {
 	token string
 }
 
-func (a *tokenAuthorizer) Authorize(req *http.Request, resourceType, resourceLink string) {
+func (a *tokenAuthorizer) Authorize(ctx context.Context, req *http.Request, resourceType, resourceLink string) {
 	req.Header.Set("Authorization", url.QueryEscape(a.token))
 }
 
 func NewTokenAuthorizer(token string) Authorizer {
 	return &tokenAuthorizer{token: token}
+}
+
+// oauthAADAuthorizer is used to generate oauth token will be used to connect to CosmosDB
+type oauthAADAuthorizer struct {
+	token *adal.ServicePrincipalToken
+}
+
+func (a *oauthAADAuthorizer) Authorize(ctx context.Context, req *http.Request, resourceType, resourceLink string) {
+	oauthToken, err := getTokenCredential(ctx, a.token)
+	if err != nil {
+		return
+	}
+	req.Header.Set("Authorization", url.QueryEscape(fmt.Sprintf("type=aad&ver=1.0&sig=%s", oauthToken)))
+
+	date := time.Now().UTC().Format("Mon, 02 Jan 2006 15:04:05 GMT")
+	req.Header.Set("x-ms-date", date)
+}
+
+func NewOauthAADAuthorizer(ctx context.Context, token *adal.ServicePrincipalToken) (Authorizer, error) {
+	return &oauthAADAuthorizer{token: token}, nil
+}
+
+// Gets a refreshed token credential to use on authorizer
+func getTokenCredential(ctx context.Context, token *adal.ServicePrincipalToken) (string, error) {
+	err := token.EnsureFreshWithContext(ctx)
+	if err != nil {
+		return "", err
+	}
+	oauthToken := token.OAuthToken()
+	return oauthToken, nil
 }
